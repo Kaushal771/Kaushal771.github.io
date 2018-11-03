77d1d2b5f146019d94fdad98613a8e680a97601c
Kaushal Desai <kaushal_desai@hotmail.com>
2018-11-03 18:58:15 -0400

    perl

---
+++ lab4.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+use CGI':standard';
+use strict;
+print "Content-type: text/html\n\n";
+
+print qq("<p style="text-align:center; fontsize:36px"> Hello this is my first perl program </p>");
